1d3eb7674adf2569cc1a8232819a3e5db8bafcb8
Jayson Fong <jaysonf1007@gmail.com>
2018-10-20 22:41:26 -0400

    Removed Unused Function

--- upload/library/jayson/DYS/Model/StyleGroup.php
+++ upload/library/jayson/DYS/Model/StyleGroup.php
@@ -7,9 +7,6 @@ class jayson_DYS_Model_StyleGroup extends XenForo_Model {
 	public function setStyleGroup($displayGroup, $userId) {
         $this->_getDb()->query('UPDATE xf_user SET jayson_displaygroup_id = ? WHERE user_id = ?', array($displayGroup, $userId));
 	}
-    protected function _getUserWriter() {
-        return XenForo_DataWriter::create('XenForo_DataWriter_User');
-    }
     protected function _getUserModel() {
         return $this->getModelFromCache('XenForo_Model_User');
     }
